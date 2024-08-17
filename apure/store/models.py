db1c0f0641b70d866ee6f48bdc33bd6e03f5eef3
asrivastavrangers <asrivastav@sandiego.edu>
2024-08-17 17:30:47 +0530

    Created a category table using MPTT

--- apure/store/models.py
+++ apure/store/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+from mptt.models import MPTTModel, TreeForeignKey
 
-# Create your models here.
+
+# Category table
+class Category(MPTTModel):
+    name = models.CharField(
+        verbose_name=_('Category Name'),
+        help_text=_('Required and Unique'),
+        max_length=255,
+        unique=True,
+    )
+    slug = models.SlugField(verbose_name=_('Category Safe Url'), max_length=255, unique=True)
+    parent = TreeForeignKey('self', on_delete = models.CASCADE, null = True, blank = True, related_name = "children")
+    is_active = models.BooleanField(default=True)
+
