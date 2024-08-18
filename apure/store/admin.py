a5031e38beffaf2256f2eebe434181a47dc26bc9
asrivastavrangers <asrivastav@sandiego.edu>
2024-08-18 12:47:51 +0530

    Added database

--- apure/store/admin.py
+++ apure/store/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+from .models import (
+    Category,
+    Product,
+    Product_Specification,
+    ProductType,
+    ProductSpecificationValue,
+    ProductImage,
+)
+
 
 # Register your models here.
+admin.site.register(Category, MPTTModelAdmin)
+admin.site.register(Product, MPTTModelAdmin)
+admin.site.register(Product_Specification, MPTTModelAdmin)
+admin.site.register(ProductType, MPTTModelAdmin)
+admin.site.register(ProductSpecificationValue, MPTTModelAdmin)
+admin.site.register(ProductImage, MPTTModelAdmin)
