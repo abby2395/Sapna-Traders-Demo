30643232d727447362119b5e955ecb06a628d3e4
asrivastavrangers <asrivastav@sandiego.edu>
2024-08-24 11:39:06 +0530

    Added the serialzer

--- apure/store/views.py
+++ apure/store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Category, Product
+from .serializers import ProductSerializer
 
 # Create your views here.
 class ProductListView(generics.ListAPIView):
